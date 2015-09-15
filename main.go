4ece5ad3f640011e0e9d7af59381edab7eb2aa90
Roberto Ortega <berto.ort@gmail.com>
2015-09-15 11:33:22 -0600

    mongo added

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
-	// "gopkg.in/mgo.v2"
-	// "gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	// "time"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"strings"
+	// "fmt"
 )
 
-// Entry is mongo structure
-// type Entry struct {
-// 	ID   bson.ObjectId `bson:"_id,omitempty"`
-// 	Name string
-// 	Time time.Time
-// }
+// Stream is the stream mongo structure
+type Stream struct {
+	ID   bson.ObjectId `bson:"_id,omitempty"`
+	Name string
+	Game string
+	URL  string
+}
 
 // Twitch is the json from top games
 type Twitch struct {
@@ -27,14 +28,22 @@ type Twitch struct {
 	Top   []map[string]interface{} `json:"top"`
 }
 
-func main() {
-	// lab := os.Getenv("MONGOLAB_URI")
+// Streams is the json from top streams
+type Streams struct {
+	Total   int                      `json:"_total"`
+	Links   map[string]interface{}   `json:"_links"`
+	Streams []map[string]interface{} `json:"streams"`
+}
 
-	// session, err := mgo.Dial(lab)
-	// col := session.DB("go-test").C("names")
-	// if err != nil {
-	// 	panic(err)
-	// }
+func main() {
+	lab := os.Getenv("MONGOLAB_URI")
+	db := os.Getenv("DATBASE_NAME")
+
+	session, err := mgo.Dial(lab)
+	col := session.DB(db).C("streams")
+	if err != nil {
+		panic(err)
+	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -46,16 +55,15 @@ func main() {
 	r.Static("/public", "public")
 
 	r.GET("/", func(c *gin.Context) {
-		// var results []Entry
-		// col.Find(nil).All(&results)
-		// c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
-		// 	"title": results,
-		// })
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		var results []Stream
+		col.Find(nil).All(&results)
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+			"streams": results,
+		})
 	})
 
 	r.GET("/browse", func(c *gin.Context) {
-		url := "https://api.twitch.tv/kraken/games/top"
+		url := "https://api.twitch.tv/kraken/games/top?limit=100"
 		req, err := http.NewRequest("GET", url, nil)
 		req.Header.Set("Content-Type", "application/json")
 		client := &http.Client{}
@@ -69,15 +77,30 @@ func main() {
 		if err := json.Unmarshal([]byte(body), &dat); err != nil {
 			panic(err)
 		}
-		// games := dat["top"].([]interface{})
-		fmt.Println(dat.Top)
 		c.HTML(http.StatusOK, "browse.tmpl.html", gin.H{
 			"games": dat.Top,
 		})
 	})
 
 	r.GET("/browse/:game", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "show.tmpl.html", nil)
+		game := c.Param("game")
+		url := "https://api.twitch.tv/kraken/search/streams?q=" + strings.Replace(game, " ", "%20", -1) + "&limit=25"
+		req, err := http.NewRequest("GET", url, nil)
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		dat := &Streams{}
+		if err := json.Unmarshal([]byte(body), &dat); err != nil {
+			panic(err)
+		}
+		c.HTML(http.StatusOK, "show.tmpl.html", gin.H{
+			"streams": dat.Streams, "game": game,
+		})
 	})
 
 	r.GET("/search", func(c *gin.Context) {
@@ -88,14 +111,16 @@ func main() {
 		c.HTML(http.StatusOK, "about.tmpl.html", nil)
 	})
 
-	// r.POST("/", func(c *gin.Context) {
-	// 	name := c.PostForm("name")
-	// 	err = col.Insert(&Entry{Name: name, Time: time.Now()})
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	c.Redirect(http.StatusMovedPermanently, "/")
-	// })
+	r.POST("/addStream", func(c *gin.Context) {
+		name := c.PostForm("name")
+		game := c.PostForm("game")
+		url := c.PostForm("url")
+		err = col.Insert(&Stream{Name: name, Game: game, URL: url})
+		if err != nil {
+			panic(err)
+		}
+		c.Redirect(http.StatusMovedPermanently, "/")
+	})
 
 	r.Run(":" + port)
 }
---
+++ public/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // $.post("/addGame", {game: "awesome", "name": "it", "url": "works"})
+  //     .done(function( data ) {
+  //     alert( "Data Loaded: " + data );
+  //   });
+})
